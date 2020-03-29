a64c845a4c4b1b737588823bb562463de1bb6f58
Xavier Araque <xavieraraque@gmail.com>
2020-03-29 17:30:15 -0400

    Update 29

--- components/nav.vue
+++ components/nav.vue
@@ -44,42 +44,42 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      pais: process.env.country,
-      usuarioGit: process.env.gitUser,
-      repositorio: process.env.gitRepotitory
+  export default {
+    data() {
+      return {
+        pais: process.env.country,
+        usuarioGit: process.env.gitUser,
+        repositorio: process.env.gitRepotitory
+      }
     }
   }
-}
-document.addEventListener('DOMContentLoaded', () => {
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll('.navbar-burger'),
-    0
-  )
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach(el => {
-      el.addEventListener('click', () => {
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target
-        const $target = document.getElementById(target)
+  document.addEventListener('DOMContentLoaded', () => {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(
+      document.querySelectorAll('.navbar-burger'),
+      0
+    )
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+      // Add a click event on each of them
+      $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+          // Get the target from the "data-target" attribute
+          const target = el.dataset.target
+          const $target = document.getElementById(target)
 
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle('is-active')
-        $target.classList.toggle('is-active')
+          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+          el.classList.toggle('is-active')
+          $target.classList.toggle('is-active')
+        })
       })
-    })
-  }
-})
+    }
+  })
 </script>
 
 <style>
-.gradient {
-  background: linear-gradient(-30deg, #2e2575, #b713a6 45%, #2e2575 45%) #361b9c;
-  box-shadow: 0px 10px 1rem 0 rgba(79, 56, 164, 0.2);
-}
+  .gradient {
+    background: linear-gradient(-30deg, #2e2575, #b713a6 45%, #2e2575 45%) #361b9c;
+    box-shadow: 0px 10px 1rem 0 rgba(79, 56, 164, 0.2);
+  }
 </style>
