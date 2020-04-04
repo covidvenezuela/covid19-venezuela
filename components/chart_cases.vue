430957df5f231f2195629698045742cf03d824a4
Xavier Araque <xavieraraque@gmail.com>
2020-04-03 20:08:29 -0400

    Update 3 April 3

--- components/chart_cases.vue
+++ components/chart_cases.vue
@@ -10,114 +10,114 @@
 </template>
 
 <script>
-  import * as am4core from '@amcharts/amcharts4/core'
-  import * as am4charts from '@amcharts/amcharts4/charts'
-  import am4themes_animated from '@amcharts/amcharts4/themes/animated'
-  import am4themes_dataviz from '@amcharts/amcharts4/themes/dataviz'
-  // Themes begin
-  am4core.useTheme(am4themes_animated)
-  am4core.useTheme(am4themes_dataviz)
-  // Themes end
+import * as am4core from '@amcharts/amcharts4/core'
+import * as am4charts from '@amcharts/amcharts4/charts'
+import am4themes_animated from '@amcharts/amcharts4/themes/animated'
+import am4themes_dataviz from '@amcharts/amcharts4/themes/dataviz'
+// Themes begin
+am4core.useTheme(am4themes_animated)
+am4core.useTheme(am4themes_dataviz)
+// Themes end
 
-  export default {
-    name: 'ChartCases',
-    data() {
-      return {
-        totalPoints: this.$store.state.dataNacional
+export default {
+  name: 'ChartCases',
+  data() {
+    return {
+      totalPoints: this.$store.state.dataNacional
+    }
+  },
+  mounted() {
+    // Create chart instance
+    var chart = am4core.create('chartdivcases', am4charts.XYChart)
+    chart.maskBullets = false
+    chart.numberFormatter.numberFormat = '#'
+
+    let estados = this.totalPoints
+
+    estados.sort(function(a, b) {
+      return a.Confirmed - b.Confirmed
+    })
+
+    chart.data = estados
+
+    chart.exporting.menu = new am4core.ExportMenu()
+    chart.exporting.menu.align = 'left'
+    chart.exporting.menu.verticalAlign = 'bottom'
+    chart.exporting.menu.items = [
+      {
+        label: '...',
+        menu: [
+          { type: 'png', label: 'PNG', options: { quality: 1, scale: 2 } },
+          { type: 'jpg', label: 'JPG', options: { quality: 1, scale: 2 } },
+          { type: 'svg', label: 'SVG' }
+        ]
       }
-    },
-    mounted() {
-      // Create chart instance
-      var chart = am4core.create('chartdivcases', am4charts.XYChart)
-      chart.maskBullets = false
-      chart.numberFormatter.numberFormat = '#'
+    ]
 
-      let estados = this.totalPoints
+    // Create axes
+    var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis())
+    categoryAxis.dataFields.category = 'Province/State'
+    categoryAxis.renderer.grid.template.location = 0
 
-      estados.sort(function(a, b) {
-        return a.Confirmed - b.Confirmed
-      })
+    var valueAxis = chart.xAxes.push(new am4charts.ValueAxis())
+    valueAxis.renderer.inside = true
+    valueAxis.renderer.labels.template.disabled = true
 
-      chart.data = estados
+    // Modify chart's colors
+    chart.colors.list = [am4core.color('#f14668'), am4core.color('#3298dc')]
+    // Create series
+    function createSeries(field, name) {
+      // Set up series
+      var series = chart.series.push(new am4charts.ColumnSeries())
+      series.name = name
+      series.dataFields.valueX = field
+      series.dataFields.categoryY = 'Province/State'
+      series.sequencedInterpolation = true
 
-      chart.exporting.menu = new am4core.ExportMenu()
-      chart.exporting.menu.align = 'left'
-      chart.exporting.menu.verticalAlign = 'bottom'
-      chart.exporting.menu.items = [
-        {
-          label: '...',
-          menu: [
-            { type: 'png', label: 'PNG', options: { quality: 1, scale: 2 } },
-            { type: 'jpg', label: 'JPG', options: { quality: 1, scale: 2 } },
-            { type: 'svg', label: 'SVG' }
-          ]
-        }
-      ]
+      // Make it stacked
+      series.stacked = true
 
-      // Create axes
-      var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis())
-      categoryAxis.dataFields.category = 'Province/State'
-      categoryAxis.renderer.grid.template.location = 0
+      // Configure columns
+      series.columns.template.width = am4core.percent(80)
+      series.columns.template.tooltipText =
+        '[bold]{name}[/]\n[font-size:14px]{categoryY}: {valueX}'
 
-      var valueAxis = chart.xAxes.push(new am4charts.ValueAxis())
-      valueAxis.renderer.inside = true
-      valueAxis.renderer.labels.template.disabled = true
+      // Add label
+      var labelBullet = series.bullets.push(new am4charts.LabelBullet())
+      labelBullet.label.text = '[bold]{valueX}[/]'
+      labelBullet.label.fill = am4core.color('#FFF')
+      labelBullet.locationY = 0.5
+      labelBullet.locationX = 0.5
 
-      // Modify chart's colors
-      chart.colors.list = [am4core.color('#f14668'), am4core.color('#3298dc')]
-      // Create series
-      function createSeries(field, name) {
-        // Set up series
-        var series = chart.series.push(new am4charts.ColumnSeries())
-        series.name = name
-        series.dataFields.valueX = field
-        series.dataFields.categoryY = 'Province/State'
-        series.sequencedInterpolation = true
+      return series
+    }
 
-        // Make it stacked
-        series.stacked = true
+    createSeries('Confirmed', 'Confirmados')
+    createSeries('Deaths', 'Fallecidos')
 
-        // Configure columns
-        series.columns.template.width = am4core.percent(80)
-        series.columns.template.tooltipText =
-          '[bold]{name}[/]\n[font-size:14px]{categoryY}: {valueX}'
+    // Create series for total
+    var totalSeries = chart.series.push(new am4charts.ColumnSeries())
+    totalSeries.dataFields.valueX = 'none'
+    totalSeries.dataFields.categoryY = 'Province/State'
+    totalSeries.stacked = true
+    totalSeries.hiddenInLegend = true
+    totalSeries.columns.template.strokeOpacity = 0
 
-        // Add label
-        var labelBullet = series.bullets.push(new am4charts.LabelBullet())
-        labelBullet.label.text = '[bold]{valueX}[/]'
-        labelBullet.label.fill = am4core.color('#FFF')
-        labelBullet.locationY = 0.5
-        labelBullet.locationX = 0.5
+    // Legend
+    chart.legend = new am4charts.Legend()
+  },
 
-        return series
-      }
-
-      createSeries('Confirmed', 'Confirmados')
-      createSeries('Deaths', 'Fallecidos')
-
-      // Create series for total
-      var totalSeries = chart.series.push(new am4charts.ColumnSeries())
-      totalSeries.dataFields.valueX = 'none'
-      totalSeries.dataFields.categoryY = 'Province/State'
-      totalSeries.stacked = true
-      totalSeries.hiddenInLegend = true
-      totalSeries.columns.template.strokeOpacity = 0
-
-      // Legend
-      chart.legend = new am4charts.Legend()
-    },
-
-    beforeDestroy() {
-      if (this.chart) {
-        this.chart.dispose()
-      }
+  beforeDestroy() {
+    if (this.chart) {
+      this.chart.dispose()
     }
   }
+}
 </script>
 
 <style>
-  .chart-cases {
-    width: 96%;
-    height: 950px;
-  }
+.chart-cases {
+  width: 96%;
+  height: 950px;
+}
 </style>
